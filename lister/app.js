8d9c15105013e7aaa71a83f2bb8d6e57c5b728d6
SAY-droid427 <sayanimallick0218@gmail.com>
2020-06-03 23:21:58 +0530

    Second commit on Lister app

--- lister/app.js
+++ lister/app.js
@@ -1,7 +1,20 @@
+//Creating the new component li
+var items=document.getElementById("card");
+
 //Checking whether the buttons are working
 var BOOT=document.querySelector("#butt");
 BOOT.addEventListener("click",toClick);
 function toClick(e){
     e.preventDefault();
     console.log("Clicked");
+    console.log(e.target.value);
+    var newItem=document.createElement("li");
+    newItem.className="list-group-item";
+    var tex=document.getElementById("control").value;
+    var newContent=document.createTextNode(tex);
+    newItem.appendChild(newContent); 
+    items.appendChild(newItem);
+    
+
+    
 }
