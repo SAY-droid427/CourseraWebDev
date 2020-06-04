9a0a142a12f7e7695260378ed415c9f1b7d4f5fe
SAY-droid427 <sayanimallick0218@gmail.com>
2020-06-04 10:24:25 +0530

    made changes in the functionality

--- lister/app.js
+++ lister/app.js
@@ -3,8 +3,8 @@ var items=document.getElementById("card");
 
 //Checking whether the buttons are working
 var BOOT=document.querySelector("#butt");
-BOOT.addEventListener("click",toClick);
-function toClick(e){
+BOOT.addEventListener("click",addItem);
+function addItem(e){
     e.preventDefault();
     console.log("Clicked");
     console.log(e.target.value);
@@ -13,8 +13,11 @@ function toClick(e){
     var tex=document.getElementById("control").value;
     var newContent=document.createTextNode(tex);
     newItem.appendChild(newContent); 
-    items.appendChild(newItem);
-    
-
-    
+    items.appendChild(newItem);    
+}
+var deleteBtn=document.getElementsByClassName("list-group-item");
+function removeItem(e){
+    e.preventDefault();
+    console.log(deleteBtn);
+  
 }
